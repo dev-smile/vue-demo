122987861545f28352061be9a2bfb3f9a4504741
dev-smile <kou2004@naver.com>
2024-06-09 16:28:30 +0900

    [feat] add menu

--- src/App.vue.js
+++ src/App.vue.js
@@ -1,4 +1,4 @@
-import { RouterView } from 'vue-router';
+import { RouterView, RouterLink } from "vue-router";
 const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, } = await import('vue');
 const __VLS_fnComponent = (await import('vue')).defineComponent({});
 let __VLS_functionalComponentProps;
@@ -14,16 +14,44 @@ function __VLS_template() {
     // CSS variable injection 
     // CSS variable injection end 
     let __VLS_resolvedLocalAndGlobalComponents;
-    const __VLS_0 = {}.RouterView;
+    __VLS_elementAsFunction(__VLS_intrinsicElements.header, __VLS_intrinsicElements.header)({});
+    __VLS_elementAsFunction(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({ ...{ class: ("wrapper") }, });
+    __VLS_elementAsFunction(__VLS_intrinsicElements.nav, __VLS_intrinsicElements.nav)({});
+    const __VLS_0 = {}.RouterLink;
+    ({}.RouterLink);
+    ({}.RouterLink);
+    __VLS_components.RouterLink;
+    __VLS_components.RouterLink;
+    // @ts-ignore
+    [RouterLink, RouterLink,];
+    const __VLS_1 = __VLS_asFunctionalComponent(__VLS_0, new __VLS_0({ to: ("/"), }));
+    const __VLS_2 = __VLS_1({ to: ("/"), }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+    ({}({ to: ("/"), }));
+    (__VLS_5.slots).default;
+    const __VLS_5 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2);
+    const __VLS_6 = {}.RouterLink;
+    ({}.RouterLink);
+    ({}.RouterLink);
+    __VLS_components.RouterLink;
+    __VLS_components.RouterLink;
+    // @ts-ignore
+    [RouterLink, RouterLink,];
+    const __VLS_7 = __VLS_asFunctionalComponent(__VLS_6, new __VLS_6({ to: ("/about"), }));
+    const __VLS_8 = __VLS_7({ to: ("/about"), }, ...__VLS_functionalComponentArgsRest(__VLS_7));
+    ({}({ to: ("/about"), }));
+    (__VLS_11.slots).default;
+    const __VLS_11 = __VLS_pickFunctionalComponentCtx(__VLS_6, __VLS_8);
+    const __VLS_12 = {}.RouterView;
     ({}.RouterView);
     __VLS_components.RouterView;
     // @ts-ignore
     [RouterView,];
-    const __VLS_1 = __VLS_asFunctionalComponent(__VLS_0, new __VLS_0({}));
-    const __VLS_2 = __VLS_1({}, ...__VLS_functionalComponentArgsRest(__VLS_1));
+    const __VLS_13 = __VLS_asFunctionalComponent(__VLS_12, new __VLS_12({}));
+    const __VLS_14 = __VLS_13({}, ...__VLS_functionalComponentArgsRest(__VLS_13));
     ({}({}));
-    const __VLS_5 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2);
+    const __VLS_17 = __VLS_pickFunctionalComponentCtx(__VLS_12, __VLS_14);
     if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+        __VLS_styleScopedClasses['wrapper'];
     }
     var __VLS_slots;
     return __VLS_slots;
@@ -33,6 +61,7 @@ function __VLS_template() {
         setup() {
             return {
                 RouterView: RouterView,
+                RouterLink: RouterLink,
             };
         },
     });
